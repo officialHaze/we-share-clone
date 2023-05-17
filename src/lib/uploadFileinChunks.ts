cf93f04d6d1296655e15caa39d83c666da1cad05
hitman <hitman@assassin.brotherhood>
2023-05-17 22:43:16 +0530

    changed the offset value part 3

--- src/lib/uploadFileinChunks.ts
+++ src/lib/uploadFileinChunks.ts
@@ -45,7 +45,7 @@ export default async function uploadFileInChunks(
             ...updateCachedData,
             file: {
               name: file.name,
-              offset: offset + chunk.length,
+              offset: offset,
             },
           };
           localStorage.setItem(
