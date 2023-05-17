a2ccd5a63d1ead7780c91588129c7b668539ee30
hitman <hitman@assassin.brotherhood>
2023-05-17 21:42:31 +0530

    making new commit from linux

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -10,292 +10,296 @@ import { storeLocalData, deleteLocalData } from "../lib/localData";
 import uploadFileInChunks from "../lib/uploadFileinChunks";
 
 // const baseUrl = "http://localhost:3000";
-const baseUrl = "https://seol-share.vercel.app";
+const baseUrl = "https://small-share.vercel.app";
 
 interface CachedUploadData {
-	file: {
-		name: string;
-		offset: number;
-	};
-	fileIndex: number;
-	title: string;
-	desc: string;
+  file: {
+    name: string;
+    offset: number;
+  };
+  fileIndex: number;
+  title: string;
+  desc: string;
 }
 
 interface InitialVal {
-	fileName: string;
-	description: string;
+  fileName: string;
+  description: string;
 }
 
 interface Props {
-	files: File[] | [];
-	uploadFiles: React.Dispatch<React.SetStateAction<File[] | []>>;
-	setHasProgressStarted: React.Dispatch<React.SetStateAction<boolean>>;
-	setProgressState: React.Dispatch<React.SetStateAction<string>>;
-	LinkToDownloadPg: React.Dispatch<React.SetStateAction<string | null>>;
-	uploadError: React.Dispatch<React.SetStateAction<boolean>>;
-	setUploadedSize: React.Dispatch<React.SetStateAction<number[]>>;
-	toResumeUpload: boolean;
-	cachedData: CachedUploadData | null;
-	values: InitialVal;
-	setInputValues: React.Dispatch<React.SetStateAction<InitialVal>>;
-	handleDetailChange: (
-		e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
-	) => void;
+  files: File[] | [];
+  uploadFiles: React.Dispatch<React.SetStateAction<File[] | []>>;
+  setHasProgressStarted: React.Dispatch<React.SetStateAction<boolean>>;
+  setProgressState: React.Dispatch<React.SetStateAction<string>>;
+  LinkToDownloadPg: React.Dispatch<React.SetStateAction<string | null>>;
+  uploadError: React.Dispatch<React.SetStateAction<boolean>>;
+  setUploadedSize: React.Dispatch<React.SetStateAction<number[]>>;
+  toResumeUpload: boolean;
+  cachedData: CachedUploadData | null;
+  values: InitialVal;
+  setInputValues: React.Dispatch<React.SetStateAction<InitialVal>>;
+  handleDetailChange: (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => void;
 }
 
 export default function FileUpload({
-	files,
-	uploadFiles,
-	setHasProgressStarted,
-	setProgressState,
-	LinkToDownloadPg,
-	uploadError,
-	setUploadedSize,
-	toResumeUpload,
-	cachedData,
-	values,
-	handleDetailChange,
+  files,
+  uploadFiles,
+  setHasProgressStarted,
+  setProgressState,
+  LinkToDownloadPg,
+  uploadError,
+  setUploadedSize,
+  toResumeUpload,
+  cachedData,
+  values,
+  handleDetailChange,
 }: Props) {
-	//empty the filelist
-	const removeFile = (i: number | null) => {
-		if (i !== null) {
-			const newFiles = files.filter((file, idx) => {
-				return idx !== i;
-			});
-			uploadFiles(newFiles);
-		} else {
-			uploadFiles([]);
-		}
-	};
+  //empty the filelist
+  const removeFile = (i: number | null) => {
+    if (i !== null) {
+      const newFiles = files.filter((file, idx) => {
+        return idx !== i;
+      });
+      uploadFiles(newFiles);
+    } else {
+      uploadFiles([]);
+    }
+  };
 
-	//check file size before setting the state
-	const checkFileSize = (files: FileList) => {
-		let totalSize = 0;
-		Array.from(files).forEach(file => {
-			totalSize += file.size;
-		});
-		if (totalSize > 500000000) {
-			return "exceeded";
-		}
-		return "in-limit";
-	};
+  //check file size before setting the state
+  const checkFileSize = (files: FileList) => {
+    let totalSize = 0;
+    Array.from(files).forEach((file) => {
+      totalSize += file.size;
+    });
+    if (totalSize > 500000000) {
+      return "exceeded";
+    }
+    return "in-limit";
+  };
 
-	//handles normal upload without any interruption
-	const normalUpload = async (randomId: string) => {
-		try {
-			const uploadDataObj = {
-				file: {
-					name: "",
-					offset: 0,
-				},
-				fileIndex: null,
-				title: `${values.fileName}_${randomId}`,
-				desc: values.description,
-			};
-			const fileId = await uploadFileInChunks(files, setUploadedSize, uploadDataObj);
-			const fileDesc = values.description
-				? values.description
-				: "No description for this file";
+  //handles normal upload without any interruption
+  const normalUpload = async (randomId: string) => {
+    try {
+      const uploadDataObj = {
+        file: {
+          name: "",
+          offset: 0,
+        },
+        fileIndex: null,
+        title: `${values.fileName}_${randomId}`,
+        desc: values.description,
+      };
+      const fileId = await uploadFileInChunks(
+        files,
+        setUploadedSize,
+        uploadDataObj
+      );
+      const fileDesc = values.description
+        ? values.description
+        : "No description for this file";
 
-			const encryptedUrl = encryptURL(
-				`${baseUrl}/download/${values.fileName}/${fileDesc}/${fileId.toString()}`,
-			); //encrypt the download page url
-			return encryptedUrl;
-		} catch (err) {
-			throw err;
-		}
-	};
+      const encryptedUrl = encryptURL(
+        `${baseUrl}/download/${
+          values.fileName
+        }/${fileDesc}/${fileId.toString()}`
+      ); //encrypt the download page url
+      return encryptedUrl;
+    } catch (err) {
+      throw err;
+    }
+  };
 
-	//handles upload after any interruption(resumes the upload)
-	const resumeUpload = async (randomId: string, cachedData: CachedUploadData) => {
-		try {
-			const idx = cachedData.fileIndex;
-			const filteredFileList = files.filter((file, i) => {
-				return i >= idx;
-			});
-			const fileId = await uploadFileInChunks(filteredFileList, setUploadedSize, cachedData);
-			const fileDesc = cachedData.desc ? cachedData.desc : "No description for this file";
+  //handles upload after any interruption(resumes the upload)
+  const resumeUpload = async (
+    randomId: string,
+    cachedData: CachedUploadData
+  ) => {
+    try {
+      const idx = cachedData.fileIndex;
+      const filteredFileList = files.filter((file, i) => {
+        return i >= idx;
+      });
+      const fileId = await uploadFileInChunks(
+        filteredFileList,
+        setUploadedSize,
+        cachedData
+      );
+      const fileDesc = cachedData.desc
+        ? cachedData.desc
+        : "No description for this file";
 
-			const encryptedUrl = encryptURL(
-				`${baseUrl}/download/${cachedData.title}/${fileDesc}/${fileId.toString()}`,
-			); //encrypt the download page url
-			return encryptedUrl;
-		} catch (err) {
-			throw err;
-		}
-	};
+      const encryptedUrl = encryptURL(
+        `${baseUrl}/download/${
+          cachedData.title
+        }/${fileDesc}/${fileId.toString()}`
+      ); //encrypt the download page url
+      return encryptedUrl;
+    } catch (err) {
+      throw err;
+    }
+  };
 
-	const handleUpload = async (e: FormEvent) => {
-		e.preventDefault();
-		let encryptedDownloadUrl: {
-			encryptedURL: string;
-			nonce: string;
-		} = {
-			encryptedURL: "",
-			nonce: "",
-		};
-		setHasProgressStarted(true);
-		setProgressState("start");
-		const randomId = uuidv4();
-		try {
-			storeLocalData(files);
-			if (!toResumeUpload) {
-				encryptedDownloadUrl = await normalUpload(randomId);
-			} else if (toResumeUpload && cachedData) {
-				encryptedDownloadUrl = await resumeUpload(randomId, cachedData);
-			}
+  const handleUpload = async (e: FormEvent) => {
+    e.preventDefault();
+    let encryptedDownloadUrl: {
+      encryptedURL: string;
+      nonce: string;
+    } = {
+      encryptedURL: "",
+      nonce: "",
+    };
+    setHasProgressStarted(true);
+    setProgressState("start");
+    const randomId = uuidv4();
+    try {
+      storeLocalData(files);
+      if (!toResumeUpload) {
+        encryptedDownloadUrl = await normalUpload(randomId);
+      } else if (toResumeUpload && cachedData) {
+        encryptedDownloadUrl = await resumeUpload(randomId, cachedData);
+      }
 
-			const { enc_short_url, nonce } = await shortenURL(encryptedDownloadUrl); //send a request to the server to shorten the long url
-			const short_url = decryptURL(enc_short_url, nonce); //decrypt the url once received from the server
-			LinkToDownloadPg(short_url);
-			deleteLocalData();
-			setProgressState("end");
-		} catch (err) {
-			console.error("Error while uploading", err);
-			localStorage.removeItem("cached_upload_data");
-			deleteLocalData();
-			uploadError(true);
-			setProgressState("error");
-		}
-	};
+      const { enc_short_url, nonce } = await shortenURL(encryptedDownloadUrl); //send a request to the server to shorten the long url
+      const short_url = decryptURL(enc_short_url, nonce); //decrypt the url once received from the server
+      LinkToDownloadPg(short_url);
+      deleteLocalData();
+      setProgressState("end");
+    } catch (err) {
+      console.error("Error while uploading", err);
+      localStorage.removeItem("cached_upload_data");
+      deleteLocalData();
+      uploadError(true);
+      setProgressState("error");
+    }
+  };
 
-	const handleFileInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-		const { files } = e.target;
-		if (files) {
-			const status = checkFileSize(files);
-			if (status !== "exceeded") {
-				Array.from(files).forEach((file, i) => {
-					uploadFiles(prevState => {
-						return [...prevState, file];
-					});
-				});
-			}
-		}
-	};
+  const handleFileInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files) {
+      const status = checkFileSize(files);
+      if (status !== "exceeded") {
+        Array.from(files).forEach((file, i) => {
+          uploadFiles((prevState) => {
+            return [...prevState, file];
+          });
+        });
+      }
+    }
+  };
 
-	return (
-		<>
-			{files.length !== 0 && (
-				<section className="file-details-section">
-					{files.map((file, i) => {
-						return (
-							<div
-								className="file-details"
-								key={i}>
-								<div>
-									<div>
-										<h4 style={{ fontSize: "0.9rem" }}>{file.name}</h4>
-									</div>
-									<div>
-										<p style={{ fontSize: "0.85rem" }}>
-											size: {fileSizeSerializer(file.size)}
-										</p>
-									</div>
-								</div>
-								<div className="file-remove-wrapper">
-									<RxCross2
-										style={{ cursor: "pointer" }}
-										onClick={() => {
-											removeFile(i);
-										}}
-									/>
-								</div>
-							</div>
-						);
-					})}
-					<div className="size-left-wrapper">
-						<p>
-							{files.length} {files.length > 1 ? "files" : "file"} selected
-						</p>
-						<p>
-							{fileSizeSerializer(
-								remainingSize(calculateTotalSize(files), 500000000),
-							)}{" "}
-							remaining
-						</p>
-					</div>
-				</section>
-			)}
-			<form
-				className="form"
-				onSubmit={handleUpload}>
-				{!toResumeUpload && (
-					<section className="file-upload-section">
-						<label
-							className="file-input-label"
-							htmlFor="file-input">
-							<div className="add-file-btn">
-								<IoMdAdd />
-							</div>
-							<input
-								id="file-input"
-								onChange={handleFileInputChange}
-								type="file"
-								value=""
-								multiple
-							/>
-						</label>
-						<div className="file-upload-text-wrapper">
-							<h3>
-								Upload files
-								<span>(max-size:500mb)</span>
-							</h3>
-						</div>
-					</section>
-				)}
-				{!toResumeUpload && (
-					<section>
-						<div>
-							<input
-								id="name"
-								value={values.fileName}
-								onChange={handleDetailChange}
-								type="text"
-								placeholder="Title"
-								required
-							/>
-						</div>
-						<div className="hr">
-							<hr />
-						</div>
-						<div>
-							<textarea
-								id="description"
-								value={values.description}
-								onChange={handleDetailChange}
-								placeholder="Description"
-							/>
-						</div>
-					</section>
-				)}
-				<section className="file-upload-btn-section">
-					{!toResumeUpload ? (
-						files.length !== 0 ? (
-							<button
-								className="file-upload-btn"
-								type="submit">
-								Get Transfer Link
-							</button>
-						) : (
-							<button
-								disabled
-								className="file-upload-btn"
-								type="submit">
-								Get Transfer Link
-							</button>
-						)
-					) : (
-						<button
-							className="file-upload-btn"
-							type="submit">
-							Resume Upload
-						</button>
-					)}
-				</section>
-			</form>
-		</>
-	);
+  return (
+    <>
+      {files.length !== 0 && (
+        <section className="file-details-section">
+          {files.map((file, i) => {
+            return (
+              <div className="file-details" key={i}>
+                <div>
+                  <div>
+                    <h4 style={{ fontSize: "0.9rem" }}>{file.name}</h4>
+                  </div>
+                  <div>
+                    <p style={{ fontSize: "0.85rem" }}>
+                      size: {fileSizeSerializer(file.size)}
+                    </p>
+                  </div>
+                </div>
+                <div className="file-remove-wrapper">
+                  <RxCross2
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      removeFile(i);
+                    }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+          <div className="size-left-wrapper">
+            <p>
+              {files.length} {files.length > 1 ? "files" : "file"} selected
+            </p>
+            <p>
+              {fileSizeSerializer(
+                remainingSize(calculateTotalSize(files), 500000000)
+              )}{" "}
+              remaining
+            </p>
+          </div>
+        </section>
+      )}
+      <form className="form" onSubmit={handleUpload}>
+        {!toResumeUpload && (
+          <section className="file-upload-section">
+            <label className="file-input-label" htmlFor="file-input">
+              <div className="add-file-btn">
+                <IoMdAdd />
+              </div>
+              <input
+                id="file-input"
+                onChange={handleFileInputChange}
+                type="file"
+                value=""
+                multiple
+              />
+            </label>
+            <div className="file-upload-text-wrapper">
+              <h3>
+                Upload files
+                <span>(max-size:500mb)</span>
+              </h3>
+            </div>
+          </section>
+        )}
+        {!toResumeUpload && (
+          <section>
+            <div>
+              <input
+                id="name"
+                value={values.fileName}
+                onChange={handleDetailChange}
+                type="text"
+                placeholder="Title"
+                required
+              />
+            </div>
+            <div className="hr">
+              <hr />
+            </div>
+            <div>
+              <textarea
+                id="description"
+                value={values.description}
+                onChange={handleDetailChange}
+                placeholder="Description"
+              />
+            </div>
+          </section>
+        )}
+        <section className="file-upload-btn-section">
+          {!toResumeUpload ? (
+            files.length !== 0 ? (
+              <button className="file-upload-btn" type="submit">
+                Get Transfer Link
+              </button>
+            ) : (
+              <button disabled className="file-upload-btn" type="submit">
+                Get Transfer Link
+              </button>
+            )
+          ) : (
+            <button className="file-upload-btn" type="submit">
+              Resume Upload
+            </button>
+          )}
+        </section>
+      </form>
+    </>
+  );
 }
 
 // const uploadFilesInChunk = async (): Promise<number> => {
--- src/lib/uploadFileinChunks.ts
+++ src/lib/uploadFileinChunks.ts
@@ -4,105 +4,111 @@ import { encryptFileDetails } from "../lib/encrypt_decrypt_data";
 import { deleteLocalData } from "./localData";
 
 interface CachedUploadData {
-	file: {
-		name: string;
-		offset: number;
-	};
-	fileIndex: number | null;
-	title: string;
-	desc: string;
+  file: {
+    name: string;
+    offset: number;
+  };
+  fileIndex: number | null;
+  title: string;
+  desc: string;
 }
 
 export default async function uploadFileInChunks(
-	files: File[],
-	setUploadedSize: React.Dispatch<React.SetStateAction<number[]>>,
-	cachedData: CachedUploadData,
+  files: File[],
+  setUploadedSize: React.Dispatch<React.SetStateAction<number[]>>,
+  cachedData: CachedUploadData
 ): Promise<number> {
-	let _newCachedData = { ...cachedData };
-	let fileId = 0;
-	let maxChunkSize = 20 * 1024 * 1024; //2 MB
-	let completeStatus: string;
-	let index = 0;
-	return new Promise(async (resolve, reject) => {
-		try {
-			for (let file of files) {
-				const updateCachedData = {
-					..._newCachedData,
-					fileIndex: index,
-				};
-				localStorage.setItem("cached_upload_data", JSON.stringify(updateCachedData)); //update the cached data file index on each iteration
-				const fileUint8Array = await fileToUint8Array(file);
-				let offset = _newCachedData.file.offset;
-				while (offset < file.size) {
-					const fileSize = file.size;
-					const remainingSize = fileSize - offset;
-					const _chunkSize = Math.min(remainingSize, maxChunkSize);
-					const chunk = fileUint8Array.slice(offset, offset + _chunkSize);
-					_newCachedData = {
-						...updateCachedData,
-						file: {
-							name: file.name,
-							offset: offset + chunk.length,
-						},
-					};
-					localStorage.setItem("cached_upload_data", JSON.stringify(_newCachedData));
-					console.log(offset);
-					const {
-						encFileName,
-						encZipName,
-						encFileDesc,
-						encFile,
-						encOffsetVal,
-						encNonce,
-					} = encryptFileDetails(
-						chunk,
-						file.name,
-						_newCachedData.title,
-						_newCachedData.desc,
-						offset.toString(),
-					);
-					if (fileSize - offset < maxChunkSize) {
-						completeStatus = "complete";
-					} else {
-						completeStatus = "incomplete";
-					}
-					const { id } = await sendFormData(
-						encFileName,
-						encZipName,
-						encFileDesc,
-						encFile, //original files data
-						encOffsetVal,
-						encNonce,
-						completeStatus,
-					);
-					// _newCachedData = {
-					// 	...updateCachedData,
-					// 	file: {
-					// 		name: files[i].name,
-					// 		offset: offset,
-					// 	},
-					// };
-					// localStorage.setItem("cached_upload_data", JSON.stringify(_newCachedData));
-					fileId = id;
-					setUploadedSize(prevState => {
-						return [...prevState, chunk.length];
-					});
-					offset += chunk.length;
-				}
-				_newCachedData = {
-					...updateCachedData,
-					file: {
-						name: file.name,
-						offset: 0,
-					},
-				};
-				index += 1;
-			}
-			localStorage.removeItem("cached_upload_data");
-			deleteLocalData();
-			resolve(fileId);
-		} catch (err) {
-			reject(err);
-		}
-	});
+  let _newCachedData = { ...cachedData };
+  let fileId = 0;
+  let maxChunkSize = 20 * 1024 * 1024; //2 MB
+  let completeStatus: string;
+  let index = 0;
+  return new Promise(async (resolve, reject) => {
+    try {
+      for (let file of files) {
+        const updateCachedData = {
+          ..._newCachedData,
+          fileIndex: index,
+        };
+        localStorage.setItem(
+          "cached_upload_data",
+          JSON.stringify(updateCachedData)
+        ); //update the cached data file index on each iteration
+        const fileUint8Array = await fileToUint8Array(file);
+        let offset = _newCachedData.file.offset;
+        while (offset < file.size) {
+          const fileSize = file.size;
+          const remainingSize = fileSize - offset;
+          const _chunkSize = Math.min(remainingSize, maxChunkSize);
+          const chunk = fileUint8Array.slice(offset, offset + _chunkSize);
+          _newCachedData = {
+            ...updateCachedData,
+            file: {
+              name: file.name,
+              offset: offset + chunk.length,
+            },
+          };
+          localStorage.setItem(
+            "cached_upload_data",
+            JSON.stringify(_newCachedData)
+          );
+          console.log(offset);
+          const {
+            encFileName,
+            encZipName,
+            encFileDesc,
+            encFile,
+            encOffsetVal,
+            encNonce,
+          } = encryptFileDetails(
+            chunk,
+            file.name,
+            _newCachedData.title,
+            _newCachedData.desc,
+            offset.toString()
+          );
+          if (fileSize - offset < maxChunkSize) {
+            completeStatus = "complete";
+          } else {
+            completeStatus = "incomplete";
+          }
+          const { id } = await sendFormData(
+            encFileName,
+            encZipName,
+            encFileDesc,
+            encFile, //original files data
+            encOffsetVal,
+            encNonce,
+            completeStatus
+          );
+          // _newCachedData = {
+          // 	...updateCachedData,
+          // 	file: {
+          // 		name: files[i].name,
+          // 		offset: offset,
+          // 	},
+          // };
+          // localStorage.setItem("cached_upload_data", JSON.stringify(_newCachedData));
+          fileId = id;
+          setUploadedSize((prevState) => {
+            return [...prevState, chunk.length];
+          });
+          offset += chunk.length;
+        }
+        _newCachedData = {
+          ...updateCachedData,
+          file: {
+            name: file.name,
+            offset: 0,
+          },
+        };
+        index += 1;
+      }
+      localStorage.removeItem("cached_upload_data");
+      deleteLocalData();
+      resolve(fileId);
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
