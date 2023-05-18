6c0b6f14ff0065518a8900646d17bba23218afea
hitman <hitman@assassin.brotherhood>
2023-05-18 21:28:39 +0530

    udated the bg image db and set favicon

--- src/lib/imageData.ts
+++ src/lib/imageData.ts
@@ -1,10 +1,10 @@
 export const images = [
-	"https://wallpapers.com/images/featured-full/doodle-9uuu2f3fnf2moy5p.jpg",
-	"https://r4.wallpaperflare.com/wallpaper/531/288/64/beach-doodle-james-zwadlo-were-all-lost-here-wallpaper-9bf70b3707b15f28132dc21330409e73.jpg",
-	"https://img.freepik.com/free-vector/flat-african-pattern-design_23-2149376292.jpg?w=1380&t=st=1683221533~exp=1683222133~hmac=3488d1d180a34c56c831d0934bfe4a4450a677c720b8bb49230e1514c056f3c0",
-	"https://wallpapercave.com/wp/wp5019060.jpg",
-	"https://free4kwallpapers.com/uploads/originals/2020/04/26/asian-war-dance-in-beautiful-costumes-wallpaper.jpg",
-	"https://r4.wallpaperflare.com/wallpaper/344/20/1/abstract-black-white-solar-system-space-hd-wallpaper-8b96ecdd03011fb975042ba94d9c4c50.jpg",
-	"https://wallpaper-house.com/data/out/9/wallpaper2you_315415.jpg",
-	"https://wallpaperaccess.com/full/118485.jpg",
+  "https://wallpapers.com/images/featured-full/doodle-9uuu2f3fnf2moy5p.jpg",
+  "https://r4.wallpaperflare.com/wallpaper/531/288/64/beach-doodle-james-zwadlo-were-all-lost-here-wallpaper-9bf70b3707b15f28132dc21330409e73.jpg",
+  "https://wallpaperaccess.com/full/207150.jpg",
+  "https://wallpapercave.com/wp/wp5019060.jpg",
+  "https://free4kwallpapers.com/uploads/originals/2020/04/26/asian-war-dance-in-beautiful-costumes-wallpaper.jpg",
+  "https://r4.wallpaperflare.com/wallpaper/344/20/1/abstract-black-white-solar-system-space-hd-wallpaper-8b96ecdd03011fb975042ba94d9c4c50.jpg",
+  "https://wallpaper-house.com/data/out/9/wallpaper2you_315415.jpg",
+  "https://wallpaperaccess.com/full/118485.jpg",
 ];
