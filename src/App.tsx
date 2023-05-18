d0d9843d7a8b2ef423908bbd57655b3491e4ac8a
hitman <hitman@assassin.brotherhood>
2023-05-18 21:13:08 +0530

    production deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import keepConnectionAlive from "./lib/keepConnectionAlive";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import PageNotFound from "./pages/404";
 import Download from "./pages/Download";
 
 export default function App() {
-	keepConnectionAlive();
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path="/"
-						element={<Home />}
-					/>
-					<Route
-						path="/download/:filename/:filedesc/:id"
-						element={<Download />}
-					/>
-					<Route
-						path="/about"
-						element={<About />}
-					/>
-					<Route
-						path="/404"
-						element={<PageNotFound />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/download/:filename/:filedesc/:id"
+            element={<Download />}
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/404" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
