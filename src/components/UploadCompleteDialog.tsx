4014550dafceb49ce76b6f21ae438042e5936e0c
hitman <hitman@assassin.brotherhood>
2023-05-18 22:31:49 +0530

    added media query for small screen viewing

--- src/components/UploadCompleteDialog.tsx
+++ src/components/UploadCompleteDialog.tsx
@@ -7,103 +7,104 @@ import { useState, useMemo } from "react";
 import Footer from "./Footer";
 
 interface InitialVal {
-	fileName: string;
-	description: string;
+  fileName: string;
+  description: string;
 }
 
 interface Props {
-	downloadPageLink: string | null;
-	totalFilesUploaded: number;
-	allFiles: File[];
-	removeFiles: React.Dispatch<React.SetStateAction<File[] | []>>;
-	inputValues: InitialVal;
-	resetInputValues: () => void;
+  downloadPageLink: string | null;
+  totalFilesUploaded: number;
+  allFiles: File[];
+  removeFiles: React.Dispatch<React.SetStateAction<File[] | []>>;
+  inputValues: InitialVal;
+  resetInputValues: () => void;
 }
 
 export default function UploadCompleteDialog({
-	downloadPageLink,
-	totalFilesUploaded,
-	allFiles,
-	removeFiles,
-	inputValues,
-	resetInputValues,
+  downloadPageLink,
+  totalFilesUploaded,
+  allFiles,
+  removeFiles,
+  inputValues,
+  resetInputValues,
 }: Props) {
-	const [isDialogHidden, hideDialog] = useState(true);
-	useMemo(() => {
-		downloadPageLink && hideDialog(false);
-	}, [downloadPageLink]);
+  const [isDialogHidden, hideDialog] = useState(true);
+  useMemo(() => {
+    downloadPageLink && hideDialog(false);
+  }, [downloadPageLink]);
 
-	return (
-		<div
-			className="completion-dialog-box"
-			style={{
-				transform: isDialogHidden ? "translateX(100%)" : "translateX(0)",
-			}}>
-			<div
-				style={{
-					padding: "1rem 1.5rem",
-					cursor: "pointer",
-					borderBottom: "1px solid rgba(122, 122, 122, 0.3)",
-					opacity: 0.35,
-				}}>
-				<h2>
-					<RxCrossCircled
-						onClick={() => {
-							resetInputValues();
-							removeFiles([]);
-							hideDialog(true);
-						}}
-					/>
-				</h2>
-			</div>
-			<div className="transfer-details-heading">
-				<div>
-					<h1>Your transfer details</h1>
-					<div className="transfer-details-subheading">
-						<p>
-							{totalFilesUploaded} {totalFilesUploaded > 1 ? "files" : "file"}{" "}
-							<BsDot /> {fileSizeSerializer(calculateTotalSize(allFiles))} <BsDot />{" "}
-							{allFiles.length !== 0
-								? "Expires in 20 days"
-								: "No files uploaded to set expiry"}
-						</p>
-					</div>
-				</div>
-			</div>
-			<div className="upload-details-container">
-				<div className="file-details-wrapper">
-					<div className="file-title-desc">
-						<div className="title-container">
-							<h4>Title</h4>
-							<p style={{ padding: "1rem 0" }}>{inputValues.fileName}</p>
-						</div>
-						<div className="desc-container">
-							<h4>Description</h4>
-							<p style={{ padding: "1rem 0" }}>{inputValues.description}</p>
-						</div>
-					</div>
-					<div className="files-uploaded-container">
-						<h4>
-							{totalFilesUploaded} {totalFilesUploaded > 1 ? "Files" : "File"}
-						</h4>
-						<ul>
-							{allFiles.map((file, i) => {
-								return (
-									<li
-										key={i}
-										className="files-uploaded">
-										<p>{file.name}</p>
-										<p style={{ color: "rgba(122, 122, 122,0.9)" }}>
-											{fileSizeSerializer(file.size)}
-										</p>
-									</li>
-								);
-							})}
-						</ul>
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+  return (
+    <div
+      className="completion-dialog-box"
+      style={{
+        transform: isDialogHidden ? "translateX(100%)" : "translateX(0)",
+      }}
+    >
+      <div
+        style={{
+          padding: "1rem 1.5rem",
+          cursor: "pointer",
+          borderBottom: "1px solid rgba(122, 122, 122, 0.3)",
+          opacity: 0.35,
+        }}
+      >
+        <h2>
+          <RxCrossCircled
+            onClick={() => {
+              resetInputValues();
+              removeFiles([]);
+              hideDialog(true);
+            }}
+          />
+        </h2>
+      </div>
+      <div className="transfer-details-heading">
+        <div>
+          <h1>Your transfer details</h1>
+          <div className="transfer-details-subheading">
+            <p>
+              {totalFilesUploaded} {totalFilesUploaded > 1 ? "files" : "file"}{" "}
+              <BsDot /> {fileSizeSerializer(calculateTotalSize(allFiles))}{" "}
+              <BsDot />{" "}
+              {allFiles.length !== 0
+                ? "Expires in 20 days"
+                : "No files uploaded to set expiry"}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="upload-details-container">
+        <div className="file-details-wrapper">
+          <div className="file-title-desc">
+            <div className="title-container">
+              <h4>Title</h4>
+              <p style={{ padding: "1rem 0" }}>{inputValues.fileName}</p>
+            </div>
+            <div className="desc-container">
+              <h4>Description</h4>
+              <p style={{ padding: "1rem 0" }}>{inputValues.description}</p>
+            </div>
+          </div>
+          <div className="files-uploaded-container">
+            <h4>
+              {totalFilesUploaded} {totalFilesUploaded > 1 ? "Files" : "File"}
+            </h4>
+            <ul>
+              {allFiles.map((file, i) => {
+                return (
+                  <li key={i} className="files-uploaded">
+                    <p>{file.name}</p>
+                    <p style={{ color: "rgba(122, 122, 122,0.9)" }}>
+                      {fileSizeSerializer(file.size)}
+                    </p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
--- src/components/UploadInProgress.tsx
+++ src/components/UploadInProgress.tsx
@@ -6,125 +6,124 @@ import { useState, useMemo } from "react";
 import "../component-styles/Upload-Progress.css";
 
 interface Props {
-	files: File[] | [];
-	progressState: string;
-	downloadPageUrl: string | null;
-	uploadError: boolean;
-	uploadedSizes: number[];
+  files: File[] | [];
+  progressState: string;
+  downloadPageUrl: string | null;
+  uploadError: boolean;
+  uploadedSizes: number[];
 }
 
 export default function UploadInProgress({
-	files,
-	progressState,
-	downloadPageUrl,
-	uploadError,
-	uploadedSizes,
+  files,
+  progressState,
+  downloadPageUrl,
+  uploadError,
+  uploadedSizes,
 }: Props) {
-	const [isLinkCopied, setIsLinkCopied] = useState(false);
-	useMemo(() => {
-		isLinkCopied &&
-			setTimeout(() => {
-				setIsLinkCopied(false);
-			}, 1000);
-	}, [isLinkCopied]);
-	return (
-		<div className="upload-progress-container">
-			{isLinkCopied && <LinkCopyConfirmation />}
-			<section className="progress-section">
-				{!downloadPageUrl ? (
-					<ProgressBar
-						filesize={calculateTotalSize(files)}
-						uploadedSizes={uploadedSizes}
-						progressState={progressState}
-						uploadError={uploadError}
-					/>
-				) : (
-					<div className="winner-img-wrapper">
-						<img
-							src="/winner.png"
-							width="60%"
-							height="60%"
-							alt="winner"
-						/>
-					</div>
-				)}
-			</section>
-			<section className="uploading-status-section">
-				{downloadPageUrl ? (
-					<div>
-						<h2>You're done!</h2>
-						<div className="file-upload-details">
-							{files.length !== 0 && <p>Total Files: {files.length}</p>}
-							{files.length !== 0 && (
-								<p>Total size: {fileSizeSerializer(calculateTotalSize(files))}</p>
-							)}
-						</div>
-						<div className="download-page-url-container">
-							<div className="download-page-url-wrapper">
-								<p>{downloadPageUrl}</p>
-							</div>
-						</div>
-						<div className="copy-link-btn-wrapper">
-							<button
-								onClick={() => {
-									navigator.clipboard.writeText(downloadPageUrl);
-									setIsLinkCopied(true);
-								}}>
-								Copy link
-							</button>
-							<p>
-								<a
-									className="back-to-home"
-									href="/">
-									Send more files?
-								</a>
-							</p>
-						</div>
-						<p className="link-expiry-warning">*Download link will expire in 20 days</p>
-					</div>
-				) : (
-					<div>
-						<h2 style={{ color: uploadError ? "red" : "black" }}>
-							{!uploadError ? "Uploading..." : "Upload Error!"}
-						</h2>
-						<div className="file-upload-details">
-							{files.length !== 0 && (
-								<p>
-									{fileSizeSerializer(
-										uploadedSizes.reduce((prevVal, currentVal) => {
-											return prevVal + currentVal;
-										}),
-									)}{" "}
-									of {fileSizeSerializer(calculateTotalSize(files))} uploaded
-								</p>
-							)}
-						</div>
-						<div className="download-page-url-container" />
-						<div className="copy-link-btn-wrapper">
-							{!uploadError ? (
-								<button disabled>Copy link</button>
-							) : (
-								<div>
-									<p>
-										<a
-											className="back-to-home"
-											style={{ fontSize: "1rem" }}
-											href="/">
-											Try again?
-										</a>
-									</p>
-									<p className="contact-dev-text">
-										*If the issue persists please contact <br />
-										<a href="mailto:moinak.dey8@gmail.com">
-											@moinak.dey8@gmail.com
-										</a>
-									</p>
-								</div>
-							)}
-						</div>
-					</div>
-				)}
-			</section>
-		</div>
-	);
+  const [isLinkCopied, setIsLinkCopied] = useState(false);
+  useMemo(() => {
+    isLinkCopied &&
+      setTimeout(() => {
+        setIsLinkCopied(false);
+      }, 1000);
+  }, [isLinkCopied]);
+  return (
+    <div className="upload-progress-container">
+      {isLinkCopied && <LinkCopyConfirmation />}
+      <section className="progress-section">
+        {!downloadPageUrl ? (
+          <ProgressBar
+            filesize={calculateTotalSize(files)}
+            uploadedSizes={uploadedSizes}
+            progressState={progressState}
+            uploadError={uploadError}
+          />
+        ) : (
+          <div className="winner-img-wrapper">
+            <img src="/winner.png" width="60%" height="60%" alt="winner" />
+          </div>
+        )}
+      </section>
+      <section className="uploading-status-section">
+        {downloadPageUrl ? (
+          <div>
+            <h2>You're done!</h2>
+            <div className="file-upload-details">
+              {files.length !== 0 && <p>Total Files: {files.length}</p>}
+              {files.length !== 0 && (
+                <p>
+                  Total size: {fileSizeSerializer(calculateTotalSize(files))}
+                </p>
+              )}
+            </div>
+            <div className="download-page-url-container">
+              <div className="download-page-url-wrapper">
+                <p>{downloadPageUrl}</p>
+              </div>
+            </div>
+            <div className="copy-link-btn-wrapper">
+              <button
+                onClick={() => {
+                  navigator.clipboard.writeText(downloadPageUrl);
+                  setIsLinkCopied(true);
+                }}
+              >
+                Copy link
+              </button>
+              <p>
+                <a className="back-to-home" href="/">
+                  Send more files?
+                </a>
+              </p>
+            </div>
+            <p className="link-expiry-warning">
+              *Download link will expire in 20 days
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h2 style={{ color: uploadError ? "red" : "black" }}>
+              {!uploadError ? "Uploading..." : "Upload Error!"}
+            </h2>
+            <div className="file-upload-details">
+              {files.length !== 0 && (
+                <p>
+                  {fileSizeSerializer(
+                    uploadedSizes.reduce((prevVal, currentVal) => {
+                      return prevVal + currentVal;
+                    })
+                  )}{" "}
+                  of {fileSizeSerializer(calculateTotalSize(files))} uploaded
+                </p>
+              )}
+            </div>
+            <div className="download-page-url-container" />
+            <div className="copy-link-btn-wrapper">
+              {!uploadError ? (
+                <button disabled>Copy link</button>
+              ) : (
+                <div>
+                  <p>
+                    <a
+                      className="back-to-home"
+                      style={{ fontSize: "1rem" }}
+                      href="/"
+                    >
+                      Try again?
+                    </a>
+                  </p>
+                  <p className="contact-dev-text">
+                    *If the issue persists please contact <br />
+                    <a href="mailto:moinak.dey8@gmail.com">
+                      @moinak.dey8@gmail.com
+                    </a>
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </section>
+    </div>
+  );
 }
